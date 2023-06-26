60d07377903037b3f1ba44883e2ab5a028a3e3ce
Kim Keiser <Kimalexanderkeiser@gmail.com>
2023-06-26 20:18:27 +0200

    Exercise: Implement a method with named and optional parameters

---
+++ ExerciseApp/Exercise/SendM.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseApp.Exercise
+{
+    internal class SendM
+    {
+        //Named and optional params
+        public static void SendMessage(string message, string recipient, bool isUrgent = false, DateTime? scheduledTime = null)
+        {
+            if (isUrgent) // if true
+            {
+                message = "URGENT: " + message; // + 'URGENT' to string
+            }
+
+            if (scheduledTime != null) // if not null
+            {
+                Console.WriteLine($"Message to {recipient} scheduled for {scheduledTime}: {message}");
+            }
+            else
+            {
+                Console.WriteLine($"Message sent to {recipient}: {message}");
+            }
+        }
+
+        // Ini
+        public SendM() 
+        {
+            SendMessage("Hello, how are you?", "John");
+
+            SendMessage("Meeting postponed", "All employees", true);
+
+            SendMessage("Reminder: project due in 2 days", "Project team", scheduledTime: DateTime.Now.AddDays(2));
+        }
+
+    }
+}
--- ExerciseApp/Program.cs
+++ ExerciseApp/Program.cs
@@ -1,7 +1,11 @@
 ﻿using ExerciseApp.Exercise;
 
-// Exercise: Implement a method that modifies a value type using ref, in, and out. Working
-// ModifyValueT modifyValueT = new ModifyValueT();
+//Exercise: Implement a struct to represent a date
+//Create a C# struct called Date that represents a date with a year, month, and day component. Working
+//DateRep dateRep = new DateRep();
 
-// Create a C# struct called Date that represents a date with a year, month, and day component.
-DateRep dateRep = new DateRep();
+// Exercise: Implement a method that modifies a value type using ref, in, and out. Working
+//ModifyValueT modifyValueT = new ModifyValueT();
+
+// Exercise: Implement a method with named and optional parameters. Working
+// SendM sendM = new SendM();
