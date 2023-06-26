24d4a05975530c6fb861ad288a284123d93fd1e0
Kim Keiser <Kimalexanderkeiser@gmail.com>
2023-06-26 20:46:30 +0200

    Exercise: Implement a class with data validation, Refactored Program.cs, created PersonX

---
+++ ExerciseApp/Exercise/PersonX.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ExerciseApp.Exercise
+{
+    internal class PersonX // Exercise: Implement a class with data validation
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+
+        // Constructor
+        public PersonX(string name, int age, string email = null)
+        {
+            Name = name;
+            Age = age;
+            Email = email;
+        }
+
+        public void Validate()
+        {
+            List<string> errors = new List<string>();
+
+            if (string.IsNullOrEmpty(Name) || Name.Length > 100)
+            {
+                errors.Add("Name must be between 1 and 100 characters\n");
+            }
+
+            if (Age < 0 || Age > 120)
+            {
+                errors.Add("Age must be between 0 and 120.\n");
+            }
+
+            if (Email != null && !Regex.IsMatch(Email, @"^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$"))
+            {
+                errors.Add("Email must be a valid email address format.\n");
+            }
+
+            if (errors.Count > 0)
+            {
+                throw new ArgumentException(string.Join(" ", errors));
+            }
+        }
+    }
+}
--- ExerciseApp/Program.cs
+++ ExerciseApp/Program.cs
@@ -4,13 +4,22 @@
 //Create a C# struct called Date that represents a date with a year, month, and day component. Working
 //DateRep dateRep = new DateRep();
 
+
+
 // Exercise: Implement a method that modifies a value type using ref, in, and out. Working
+
 //ModifyValueT modifyValueT = new ModifyValueT();
 
+
+
 // Exercise: Implement a method with named and optional parameters. Working
+
 // SendM sendM = new SendM();
 
+
+
 // Exercise: Implement a method that deconstructs an object into tuple values. Working
+
 /*var person = new Person
 {
     Name = "John",
@@ -22,3 +31,42 @@
 var (name, age, occupation, salary) = person.GetPersonInfo();
 
 Console.WriteLine($"{name} is {age} years old and works as a {occupation}, earning {salary:C} per year.");*/
+
+
+
+// Exercise: Implement a class with data validation. Working
+
+/*var person1 = new PersonX("John", 30, "john@example.com");
+person1.Validate(); // no exception
+
+var person2 = new PersonX(null, 150);
+try
+{
+    person2.Validate();
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine(ex.Message); // "Name must be between 1 and 100 characters."
+}
+
+var person3 = new PersonX("Jane", -10, "jane@example.com");
+try
+{
+    person3.Validate();
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine(ex.Message); // "Age must be between 0 and 120."
+}
+
+var person4 = new PersonX("Bob", 40, "invalid-email");
+try
+{
+    person4.Validate();
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine(ex.Message); // "Email must be a valid email address format."
+}*/
+
+
